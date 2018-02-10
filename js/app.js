fca1be771ec81610111d09889b5a2a29015f01c3
mirelatv <noelia.mirela@gmail.com>
2018-02-10 17:50:49 -0500

    Subiendo avance de Portafolio

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//agregando texto con efecto maquina
+
+var mensaje = "Hola, soy Noelia Tello - Front End developer Jr.", pausa = 200; 
+function inicio(){ 
+ var i = 0;
+ var m = mensaje.split(''); 
+ var t = setInterval( 
+ function(){ 
+ if(i >= m.length-1)clearInterval(t); 
+ document.getElementById('efecto').innerHTML+=m[i];
+ i++; 
+ }, pausa); 
+} 
+window.onload = inicio; 
