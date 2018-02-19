fd92fb2ac900e10a8a8946e7558c8c988ebf9177
mirelatv <noelia.mirela@gmail.com>
2018-02-19 00:33:03 -0500

    Subiendo avance al 90% de Portafolio

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //agregando texto con efecto maquina
 
-var mensaje = " Noelia Tello - Front End developer Jr.", pausa = 200; 
+var mensaje = " Noelia Tello Vara   Front End developer.", pausa = 200; 
 function inicio(){ 
  var i = 0;
  var m = mensaje.split(''); 
@@ -8,7 +8,10 @@ function inicio(){
  function(){ 
  if(i >= m.length-1)clearInterval(t); 
  document.getElementById('efecto').innerHTML+=m[i];
+ document.getElementById('efectoMovile').innerHTML+=m[i];
  i++; 
  }, pausa); 
 } 
+ 
 window.onload = inicio; 
+
